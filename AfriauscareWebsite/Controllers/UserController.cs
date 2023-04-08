48f65b7494f1a1cbd220b4b8cf2bceeacdd3c644
chero <chero@DESKTOP-N98ER7Q>
2023-04-09 08:49:33 +1000

    First master version

---
+++ AfriauscareWebsite/Controllers/UserController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AfriauscareWebsite.App_Data;
+using AfriauscareWebsite.Models;
+
+namespace AfriauscareWebsite.Controllers
+{
+    public class UserController : Controller
+    {
+        AfriAusEntities objDatabaseEntities = new AfriAusEntities();
+        // GET: User
+        public ActionResult Login()
+        {
+            UserModel objUserModel = new UserModel();
+            return View(objUserModel);
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel objUserModel)
+        {
+            if(ModelState.IsValid)
+            {
+                if(objDatabaseEntities.Users.Where(m => m.UserEmail == objUserModel.UserEmail && m.UserPassword == objUserModel.UserPassword).FirstOrDefault() == null)
+                {
+                    ModelState.AddModelError("Error","The user email and password does not match or do not exists.");
+                    return View();
+                }
+                else
+                {
+                    Session["Email"] = objUserModel.UserEmail;
+                    return RedirectToAction("Index", "HomeAdminPortal");
+                }
+
+            }
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "HomeAdminPortal");
+        }
+
+        public ActionResult CreateUser()
+        {
+            UserModel objUserModel = new UserModel();
+            objUserModel.UserActive = true;
+            return View(objUserModel);
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(UserModel objUserModel)
+        {
+            if(ModelState.IsValid)
+            {
+                User objUserEntity = new User
+                {
+                    Username = objUserModel.Username,
+                    UserLastName = objUserModel.UserLastName,
+                    UserEmail = objUserModel.UserEmail,
+                    UserPassword = objUserModel.UserPassword,
+                    UserActive = objUserModel.UserActive
+                };
+
+                objDatabaseEntities.Users.Add(objUserEntity);
+                objDatabaseEntities.SaveChanges();
+
+                objUserModel.Username = string.Empty;
+                objUserModel.UserLastName = string.Empty;
+                objUserModel.UserEmail = string.Empty;
+                objUserModel.UserPassword = string.Empty;
+                objUserModel.UserActive = false;
+                objUserModel.message = "User added successfully!";
+                return View(objUserModel);
+            }
+            return View("CreateUser");
+                
+        }
+    }
+}
---
+++ AfriauscareWebsite/Models/UserModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AfriauscareWebsite.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+
+        [DisplayName("First Name :")]
+        public string Username { get; set; }
+
+        [DisplayName("Last Name :")]
+        public string UserLastName { get; set;}
+
+        [DisplayName("Email :")]
+        [Required(ErrorMessage ="Email is Required.")]
+        public string UserEmail { get; set; }
+
+        [DisplayName("Password :")]
+        [Required(ErrorMessage ="Password is Required.")]
+        [DataType(DataType.Password)]
+        public string UserPassword { get; set; }
+
+        [DisplayName("User Active?")]
+        public Boolean UserActive { get; set; }
+
+        public string message { get; set; }
+    }
+}
