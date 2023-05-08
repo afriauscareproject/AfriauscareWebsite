6e81cd77897d2d0c254d3f14891e540093728bae
chero <chero@DESKTOP-N98ER7Q>
2023-05-08 18:55:22 +1000

    Contact Information controller functions and views

--- AfriauscareWebsite/Controllers/HomeAdminPortalController.cs
+++ AfriauscareWebsite/Controllers/HomeAdminPortalController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Afriauscare.BusinessLayer.ContactInformation;
 
 namespace AfriauscareWebsite.Controllers
 {
@@ -27,5 +28,16 @@ namespace AfriauscareWebsite.Controllers
             return View();
         }
 
+        public ActionResult ViewContactInformation()
+        {
+            List<ContactInformationModel> list = new List<ContactInformationModel>();
+
+            return View(list);
+        }
+
+        public ActionResult CreateContactInformation()
+        {
+            return View();
+        }
     }
 }
---
+++ BusinessLayer/ContactInformation/ContactInformationModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Afriauscare.BusinessLayer.ContactInformation
+{
+    public class ContactInformationModel
+    {
+        public int Contact_id { get; set; }
+
+        [DisplayName("Phone Number")]
+        public string Phone_number { get; set; }
+
+        [DisplayName("Mobile Number")]
+        public string Mobile_number { get; set; }
+
+        [DisplayName("Fax Number")]
+        public string Fax_number { get; set; }
+
+        [DisplayName("Email Address")]
+        public string Email_address { get; set; }
+
+        [DisplayName("Address")]
+        public string Contact_address { get; set; }
+
+        [DisplayName("State")]
+        public int State_id { get; set; }
+
+        [DisplayName("Suburb")]
+        public int Suburb_id { get; set; }
+
+        [DisplayName("Postcode")]
+        public string Postcode { get; set; }
+    }
+}
