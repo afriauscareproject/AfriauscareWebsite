40ae7a81c310e9c0d999e72f429700dfad361292
chero <chero@DESKTOP-N98ER7Q>
2023-05-09 12:22:28 +1000

    Session validation

--- AfriauscareWebsite/Controllers/SessionController.cs
+++ AfriauscareWebsite/Controllers/SessionController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace AfriauscareWebsite.Controllers
 {
@@ -64,8 +65,10 @@ namespace AfriauscareWebsite.Controllers
 
         public ActionResult EndSession()
         {
+            FormsAuthentication.SignOut();
+            Session.Clear();
             Session.Abandon();
-            return View();
+            return RedirectToAction("Login","Session");
         }
     }
 }
