ab05f8312d176e5b4b4a28e8acbd65f4215977ad
sirivetchla <46905302+sirivetchla@users.noreply.github.com>
2023-05-22 20:27:53 +1000

    Addedd required webpages From Gopi

--- AfriauscareWebsite/Controllers/HomeController.cs
+++ AfriauscareWebsite/Controllers/HomeController.cs
@@ -26,5 +26,11 @@ namespace AfriauscareWebsite.Controllers
 
             return View();
         }
+        public ActionResult Donate()
+        {
+
+            return View();
+        }
+       
     }
 }
